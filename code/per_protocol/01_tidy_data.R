a9bc0a0c9d07299feadfb784a5f3decc05cdb2ad
Lucas Veras <lucasdsveras@gmail.com>
2019-07-11 17:30:20 +0100

    Initial commit; script to tidy data for per protocol analysis

---
+++ code/per_protocol/01_tidy_data.R
@@ -0,0 +1,37 @@
+# Load packages and required files ----------------------------------------
+
+library(tidyverse)
+source("code/intention_to_treat/01_tidy_data.R")
+
+# Select IDs by attendance rate -------------------------------------------
+
+# Read training attendance data
+attendance <- read_csv("data/BaSEIB_attendance_rate.csv") %>% 
+  na.omit()
+
+IDs_attendance_50 <- attendance$ID[which(attendance$attend_rate_3rd_eval >= 50)]
+
+selected_IDs <- intersect(unique(whole_day$ID), IDs_attendance_50)
+
+# Read and tidy PA data ------------------------------------------------------
+
+# Filter IDs with selected attendance rate
+whole_day_50_attend_exercise <- whole_day %>% 
+  filter(ID %in% selected_IDs)
+
+whole_day_50_attend_control <- whole_day %>% 
+  filter(group == "control")
+
+whole_day_50_attend <- rbind(whole_day_50_attend_exercise, whole_day_50_attend_control) %>% 
+  arrange(eval, ID)
+
+# Read and tidy anthropometric data ---------------------------------------
+
+ant_50_attend_exercise <- ant %>%
+  filter(ID %in% selected_IDs)
+
+ant_50_attend_control <- ant %>% 
+  filter(group == "control")
+
+ant_50_attend <- rbind(ant_50_attend_exercise, ant_50_attend_control) %>% 
+  arrange(eval, ID)
