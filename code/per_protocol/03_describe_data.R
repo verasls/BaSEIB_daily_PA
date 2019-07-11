898c9bee7511eef79509270212cdf04cc063fc68
Lucas Veras <lucasdsveras@gmail.com>
2019-07-11 18:12:49 +0100

    Exclude 1st eval and 2nd eval with both groups from the analysis

--- code/per_protocol/03_describe_data.R
+++ code/per_protocol/03_describe_data.R
@@ -5,44 +5,6 @@ source("code/per_protocol/01_tidy_data.R")
 
 # Physical activity descriptives ------------------------------------------
 
-# ** 1st eval, both groups ------------------------------------------------
-
-PA_desc_1st <- whole_day_50_attend %>% 
-  filter(eval == "1st") %>% 
-  summarise(
-    SED_mean     = round(mean(avg_SED), 1),
-    SED_sd       = round(sd(avg_SED), 1),
-    LPA_mean     = round(mean(avg_LPA), 1),
-    LPA_sd       = round(sd(avg_LPA), 1),
-    MPA_median   = round(median(avg_MPA), 1),
-    MPA_iqr      = round(IQR(avg_MPA), 1),
-    VPA_median   = round(median(avg_VPA), 1),
-    VPA_iqr      = round(IQR(avg_VPA), 1),
-    MVPA_median  = round(median(avg_MVPA), 1),
-    MVPA_iqr     = round(IQR(avg_MVPA), 1),
-    steps_median = round(median(avg_steps), 1),
-    steps_iqr    = round(IQR(avg_steps), 1)
-  )
-
-# ** 2nd eval, both groups ------------------------------------------------
-
-PA_desc_2nd <- whole_day_50_attend %>% 
-  filter(eval == "2nd") %>% 
-  summarise(
-    SED_mean     = round(mean(avg_SED), 1),
-    SED_sd       = round(sd(avg_SED), 1),
-    LPA_mean     = round(mean(avg_LPA), 1),
-    LPA_sd       = round(sd(avg_LPA), 1),
-    MPA_median   = round(median(avg_MPA), 1),
-    MPA_iqr      = round(IQR(avg_MPA), 1),
-    VPA_median   = round(median(avg_VPA), 1),
-    VPA_iqr      = round(IQR(avg_VPA), 1),
-    MVPA_median  = round(median(avg_MVPA), 1),
-    MVPA_iqr     = round(IQR(avg_MVPA), 1),
-    steps_median = round(median(avg_steps), 1),
-    steps_iqr    = round(IQR(avg_steps), 1)
-  )
-
 # ** 2nd eval, control group ----------------------------------------------
 
 PA_desc_2nd_ctr <- whole_day_50_attend %>% 
@@ -121,42 +83,6 @@ PA_desc_3rd_exe <- whole_day_50_attend %>%
   )
 
 # Anthropometric descriptives ---------------------------------------------
-
-# ** 1st eval, both groups ------------------------------------------------
-
-ant_desc_1st <- ant_1st_50_attend %>% 
-  summarise(
-    age_mean = round(mean(age), 1),
-    age_sd = round(sd(age), 1),
-    height_mean = round(mean(height), 1),
-    height_sd = round(sd(height), 1),
-    body_mass_mean = round(mean(body_mass), 1),
-    body_mass_sd = round(sd(body_mass), 1),
-    BMI_mean = round(mean(BMI), 1),
-    BMI_sd = round(sd(BMI), 1),
-    waist_circ_mean = round(mean(waist_circ), 1),
-    waist_circ_sd = round(sd(waist_circ), 1),
-    hip_circ_mean = round(mean(hip_circ), 1),
-    hip_circ_sd = round(sd(hip_circ), 1)
-  )
-
-# ** 2nd eval, both groups ------------------------------------------------
-
-ant_desc_2nd <- ant_2nd_50_attend %>% 
-  summarise(
-    age_mean = round(mean(age), 1),
-    age_sd = round(sd(age), 1),
-    height_mean = round(mean(height), 1),
-    height_sd = round(sd(height), 1),
-    body_mass_mean = round(mean(body_mass), 1),
-    body_mass_sd = round(sd(body_mass), 1),
-    BMI_mean = round(mean(BMI), 1),
-    BMI_sd = round(sd(BMI), 1),
-    waist_circ_mean = round(mean(waist_circ), 1),
-    waist_circ_sd = round(sd(waist_circ), 1),
-    hip_circ_mean = round(mean(hip_circ), 1),
-    hip_circ_sd = round(sd(hip_circ), 1)
-  )
 
 # ** 2nd eval, control group ----------------------------------------------
 
