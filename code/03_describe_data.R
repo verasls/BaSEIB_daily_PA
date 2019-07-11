b3d1637c533bd6c507892c96fae9db498ed106fc
Lucas Veras <lucasdsveras@gmail.com>
2019-07-11 16:14:34 +0100

    Add anthropometric data

--- code/03_describe_data.R
+++ code/03_describe_data.R
@@ -119,3 +119,117 @@ PA_desc_3rd_exe <- whole_day %>%
     steps_median = round(median(avg_steps), 1),
     steps_iqr    = round(IQR(avg_steps), 1)
   )
+
+# Anthropometric descriptives ---------------------------------------------
+
+# ** 1st eval, both groups ------------------------------------------------
+
+ant_desc_1st <- ant_1st %>% 
+  summarise(
+    age_mean = round(mean(age), 1),
+    age_sd = round(sd(age), 1),
+    height_mean = round(mean(height), 1),
+    height_sd = round(sd(height), 1),
+    body_mass_mean = round(mean(body_mass), 1),
+    body_mass_sd = round(sd(body_mass), 1),
+    BMI_mean = round(mean(BMI), 1),
+    BMI_sd = round(sd(BMI), 1),
+    waist_circ_mean = round(mean(waist_circ), 1),
+    waist_circ_sd = round(sd(waist_circ), 1),
+    hip_circ_mean = round(mean(hip_circ), 1),
+    hip_circ_sd = round(sd(hip_circ), 1)
+  )
+
+# ** 2nd eval, both groups ------------------------------------------------
+
+ant_desc_2nd <- ant_2nd %>% 
+  summarise(
+    age_mean = round(mean(age), 1),
+    age_sd = round(sd(age), 1),
+    height_mean = round(mean(height), 1),
+    height_sd = round(sd(height), 1),
+    body_mass_mean = round(mean(body_mass), 1),
+    body_mass_sd = round(sd(body_mass), 1),
+    BMI_mean = round(mean(BMI), 1),
+    BMI_sd = round(sd(BMI), 1),
+    waist_circ_mean = round(mean(waist_circ), 1),
+    waist_circ_sd = round(sd(waist_circ), 1),
+    hip_circ_mean = round(mean(hip_circ), 1),
+    hip_circ_sd = round(sd(hip_circ), 1)
+  )
+
+# ** 2nd eval, control group ----------------------------------------------
+
+ant_desc_2nd_ctr <- ant_2nd %>% 
+  filter(group == "control") %>% 
+  summarise(
+    age_mean = round(mean(age), 1),
+    age_sd = round(sd(age), 1),
+    height_mean = round(mean(height), 1),
+    height_sd = round(sd(height), 1),
+    body_mass_mean = round(mean(body_mass), 1),
+    body_mass_sd = round(sd(body_mass), 1),
+    BMI_mean = round(mean(BMI), 1),
+    BMI_sd = round(sd(BMI), 1),
+    waist_circ_mean = round(mean(waist_circ), 1),
+    waist_circ_sd = round(sd(waist_circ), 1),
+    hip_circ_mean = round(mean(hip_circ), 1),
+    hip_circ_sd = round(sd(hip_circ), 1)
+  )
+
+# ** 2nd eval, exercise group ---------------------------------------------
+
+ant_desc_2nd_exe <- ant_2nd %>% 
+  filter(group == "exercise") %>% 
+  summarise(
+    age_mean = round(mean(age), 1),
+    age_sd = round(sd(age), 1),
+    height_mean = round(mean(height), 1),
+    height_sd = round(sd(height), 1),
+    body_mass_mean = round(mean(body_mass), 1),
+    body_mass_sd = round(sd(body_mass), 1),
+    BMI_mean = round(mean(BMI), 1),
+    BMI_sd = round(sd(BMI), 1),
+    waist_circ_mean = round(mean(waist_circ), 1),
+    waist_circ_sd = round(sd(waist_circ), 1),
+    hip_circ_mean = round(mean(hip_circ), 1),
+    hip_circ_sd = round(sd(hip_circ), 1)
+  )
+
+# ** 3rd eval, control group ----------------------------------------------
+
+ant_desc_3rd_ctr <- ant_3rd %>% 
+  filter(group == "control") %>% 
+  summarise(
+    age_mean = round(mean(age), 1),
+    age_sd = round(sd(age), 1),
+    height_mean = round(mean(height), 1),
+    height_sd = round(sd(height), 1),
+    body_mass_mean = round(mean(body_mass), 1),
+    body_mass_sd = round(sd(body_mass), 1),
+    BMI_mean = round(mean(BMI), 1),
+    BMI_sd = round(sd(BMI), 1),
+    waist_circ_mean = round(mean(waist_circ, na.rm = TRUE), 1),
+    waist_circ_sd = round(sd(waist_circ, na.rm = TRUE), 1),
+    hip_circ_mean = round(mean(hip_circ), 1),
+    hip_circ_sd = round(sd(hip_circ), 1)
+  )
+
+# ** 3rd eval, exercise group ---------------------------------------------
+
+ant_desc_3rd_exe <- ant_3rd %>% 
+  filter(group == "exercise") %>% 
+  summarise(
+    age_mean = round(mean(age), 1),
+    age_sd = round(sd(age), 1),
+    height_mean = round(mean(height), 1),
+    height_sd = round(sd(height), 1),
+    body_mass_mean = round(mean(body_mass), 1),
+    body_mass_sd = round(sd(body_mass), 1),
+    BMI_mean = round(mean(BMI), 1),
+    BMI_sd = round(sd(BMI), 1),
+    waist_circ_mean = round(mean(waist_circ), 1),
+    waist_circ_sd = round(sd(waist_circ), 1),
+    hip_circ_mean = round(mean(hip_circ), 1),
+    hip_circ_sd = round(sd(hip_circ), 1)
+  )
